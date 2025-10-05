a7224ac04aed6cdc2d857f79a3a72a6defba7b45
Swarn Pratap Singh <swarnpratapsingh5@gmail.com>
2025-10-04 19:21:16 -0500

    icon changed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import favicon from './assets/icon-mascot.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,17 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Set favicon to mascot image (imported asset) so bundler handles the hashed path
+if (typeof document !== 'undefined') {
+  try {
+    const link = document.querySelector("link[rel~='icon']") || document.createElement('link');
+    link.rel = 'icon';
+    link.href = favicon;
+    if (!document.querySelector("link[rel~='icon']")) {
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+  } catch (e) {
+    // noop
+  }
+}
